8e291fc403a3e94e6eca2274f2d61dd57eb6d23f
lionel_lints <lionel.lints@gmail.com>
2016-11-17 22:22:16 -0800

    initial commit 🍀

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+
+import initializeDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
+
+let app = express();
+
+app.server = http.createServer(app);
+
+// 3rd party middleware
+app.use(cors({
+  exposedHeaders: config.corsHeaders
+}));
+
+app.use(bodyParser.json({
+  limit : config.bodyLimit
+}));
+
+// 3rd party security stuff:
+app.use(helmet());
+
+// connect to db
+initializeDb( db => {
+  // internal middleware
+  app.use(middleware({ config, db }));
+  // api router
+  app.use('/api', api({ config, db }));
+  app.server.listen(process.env.PORT || config.port);
+  console.log(`Started on port ${app.server.address().port}`);
+});
+
+export default app;
