6bc4257e539b265eca7e12bf578dba01a4001b1a
lionel_lints <lionel.lints@gmail.com>
2016-11-18 16:46:31 -0800

    add knex into callback and update db file

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,10 @@
-// Update with your config settings.
-
-module.exports = {
-
+export default {
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/flow_todo_dev'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
-
 };
--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 export default callback => {
   // connect to a database if needed, then pass it to `callback`:
   const environment = process.env.NODE_ENV || 'development';
-  
-  callback();
+  const config = require('../knexfile')[environment];
+  const knex = require('knex')(config);  
+ 
+  callback(knex);
 }
