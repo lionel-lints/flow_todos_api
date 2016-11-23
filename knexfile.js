1eeb9f57cc88c08b1e0fa93b88990aae0ffd24eb
lionel_lints <lionel.lints@gmail.com>
2016-11-22 22:18:21 -0800

    add knexfile and package.json stuff

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,20 @@ const config = {
   development: {
     client: 'pg',
     connection: {
-      database: 'flow_todo_dev'
+      database: 'flow_todo_dev',
     },
   },
   test: {
     client: 'pg',
     connection: {
-      database: 'postgres://localhost/flow_todo_test'
+      database: 'postgres://localhost/flow_todo_test',
     },
-    debug:true
+    debug: true,
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
+    connection: process.env.DATABASE_URL,
+  },
 };
 
 export default config;
